a0233bd328a4e215307bd6926f313cb7af981a0f
emmantt <156514750+emmantt@users.noreply.github.com>
2024-10-15 10:11:01 -0400

    Update UpdateChecker.php

--- src/UpdateChecker.php
+++ src/UpdateChecker.php
@@ -3,7 +3,7 @@
 namespace leadid\simple_php_framework;
 
 class UpdateChecker {
-    public function execute() {
+    public static function execute() {
         $url = 'http://updates.lid.wallclip.me/check-update';
         $response = file_get_contents($url);
     }
